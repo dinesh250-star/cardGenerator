b625da1ebbfd51461e163ffa2d90d03f7f305d89
Dinesh Lakshmanan <dineshlakshman2001@gmail.com>
2023-06-07 00:38:15 +0530

    fixed errors

--- app/src/Components/Home.jsx
+++ app/src/Components/Home.jsx
@@ -3,38 +3,42 @@ import { useState } from "react";
 import axios from "Axios";
 import "bootstrap/dist/css/bootstrap.css";
 import HomeCss from "./Home.module.css";
+import { useEffect } from "react";
 const Home = () => {
   const [cardNo, setCardNo] = useState(); //Stores no of card to be displayed
-  const [data, setData] = useState([]); //Stores all the json data
+  const [data, setData] = useState([]); //Stores data which has to be displayed
+  const [data2, setData2] = useState([]); //Stores all the json data
   const [Display, setDisplay] = useState(false); //Render when quotes are generated
   const loopData = []; //For storing randomly generated quotes
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await axios
+        .get("http://localhost:3031/list")
+        .then((res) => {
+          console.log(res.data);
+          setData2(res.data);
+        });
+    };
 
+    fetchData();
+  }, []);
   const fetchInput = (e) => {
     setCardNo(e.target.value);
   };
 
   const submitHandler = async (e) => {
     e.preventDefault();
-    console.log(cardNo);
 
-    //  fetching all the data,then generating quotes randomly in for loop
-    //  and pushing it into loopData array and saving it. Update Display
-    //  to true so now it can be conditionally rendered
+    for (let i = 0; i < cardNo; i++) {
+      const randomNo = Math.floor(Math.random() * 100);
 
-    await axios
-      .get("http://localhost:3031/list")
-      .then((res) => {
-        setData(res.data);
-        for (let i = 0; i < cardNo; i++) {
-          const randomNo = Math.round(Math.random() * 100 + 1);
-          console.log(randomNo);
-          loopData.push(res.data[randomNo - 1]);
-        }
-        console.log(loopData);
-        setData(loopData);
-        setDisplay(true);
-      })
-      .catch((err) => console.log(err));
+      console.log(randomNo);
+      loopData.push(data2[randomNo]);
+      console.log(data2[randomNo]);
+    }
+    console.log(loopData);
+    setData(loopData);
+    setDisplay(true);
   };
 
   return (
@@ -67,13 +71,13 @@ const Home = () => {
         {Display ? (
           data.map((i) => {
             return (
-              <div class="card-columns mb-5">
-                <div class="card p-3 ">
-                  <div class="card-body">
-                    <h4 class="card-title  text-danger mb-3 display-7">
+              <div className="card-columns mb-5">
+                <div className="card p-3 ">
+                  <div className="card-body">
+                    <h4 className="card-title  text-danger mb-3 display-7">
                       {i.author}
                     </h4>
-                    <p class="card-text display-6">{i.quote}</p>
+                    <p className="card-text display-6">{i.quote}</p>
                   </div>
                 </div>
               </div>
